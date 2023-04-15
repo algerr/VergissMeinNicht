08952d2c770c95cee8e1c242a95fe54d476646e0
algerr <65679099+algerr@users.noreply.github.com>
2023-04-15 22:40:37 +0200

    Update authentifizierung.js

--- Firestore_Function_Backend/Router/authentifizierung.js
+++ Firestore_Function_Backend/Router/authentifizierung.js
@@ -4,7 +4,7 @@ var express = require('express')
 // Der Router wird mithilfe des Express-Moduls erstellt.
 var router = express.Router()
 
-// Die Funktionen werden aus dem Verwaltungs-Modul werden importiert, um sie mit den entsprechenden Routen zu verknüpfen.
+// Die Funktionen werden aus dem Verwaltungs-Modul importiert, um sie mit den entsprechenden Routen zu verknüpfen.
 const { anmeldung, registrierung, passwortAktualisieren, accountLoeschen, emailAktualisieren } = require('../Verwaltung/authentifizierung')
 
 // Die Routen werden definiert, indem die entsprechenden Verwaltungs-Funktionen mit HTTP-Methoden und Pfaden verknüpft werden.
