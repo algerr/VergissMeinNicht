dd5879558327c96b06ec761c671295f93090013f
algerr <65679099+algerr@users.noreply.github.com>
2023-04-15 22:41:30 +0200

    Update passwort.js

--- Firestore_Function_Backend/Router/passwort.js
+++ Firestore_Function_Backend/Router/passwort.js
@@ -4,16 +4,16 @@ var express = require('express')
 // Ein Router wird mithilfe des Express-Moduls erstellt.
 var router = express.Router()
 
-// Die Verwaltuns-Funktionen für die verschiedenen Routen werden aus der Datei '../Verwaltung/passwort.js' importiert.
+// Die Verwaltungsfunktionen für die verschiedenen Routen werden aus der Datei '../Verwaltung/passwort.js' importiert.
 const { passwortHinzufuegen, passwortLoeschen, allePasswoerter } = require('../Verwaltung/passwort')
 
-// Die verschiedenen Routen werden definiert, indem die entsprechenden Verwaltungs-Funktionen mit HTTP-Methoden und Pfaden verknüpft werden.
+// Die verschiedenen Routen werden definiert, indem die entsprechenden Verwaltungsfunktionen mit HTTP-Methoden und Pfaden verknüpft werden.
 
 // Verwaltuns-Funktion für das Hinzufügen eines Passwortes.
 router.post('/', passwortHinzufuegen) 
-// Verwaltuns-Funktion für das Löschen eines Passworts anhand der ID.
+// Verwaltuns-Funktion für das Löschen eines Passwortes anhand der ID.
 router.delete('/:passwortId', passwortLoeschen) 
-// Verwaltuns-Funktion für das Abrufen aller Passwörter.
+// Verwaltunfs-Funktion für das Abrufen aller Passwörter.
 router.get('/', allePasswoerter) 
 
 // Der Router wird exportiert, um ihn in der Hauptanwendung verwenden zu können.
