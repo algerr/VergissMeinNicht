42bd2bd2eff703b8ff141cdbf507458e86d64be2
algerr <65679099+algerr@users.noreply.github.com>
2023-04-15 23:06:09 +0200

    Update AccountEinstellungen.js

--- Vergissmeinnicht_Frontend/src/Komponenten/AccountEinstellungen/AccountEinstellungen.js
+++ Vergissmeinnicht_Frontend/src/Komponenten/AccountEinstellungen/AccountEinstellungen.js
@@ -1,3 +1,4 @@
+// Importieren der benötigten React-Komponenten.
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 import { connect } from 'react-redux'
@@ -6,6 +7,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 
+// Import der benötigten Komponenten und Funktionen.
 import { tokenEntschluesseln } from '../../Hilfsfunktionen/token'
 import { setzeInhaltFuerZentriertesModalfenster, zentriertesModalfensterAnzeigen, setzeInhaltFuerOberesModalfenster, oberesModalfensterAnzeigen, oberesModalfensterAusblenden } from '../../ReduxStore/aktionsErzeuger/modalFenster'
 import EmailAktualisieren from './EmailAktualisieren'
@@ -15,12 +17,13 @@ import { authentifizierungsTokenFestlegen } from '../../ReduxStore/aktionsErzeug
 
 class AccountEinstellungen extends Component {
 
-    // Das modale Fenster zur Aktualisierung der Emailadresse wird geöffnet,
+    // Das Modalfenster zur Aktualisierung der Emailadresse wird geöffnet,
     // wenn der Nutzer auf den entsprechenden Button drückt.
     onEmailAktualisieren = () => {
         const { setzeInhaltFuerZentriertesModalfenster, zentriertesModalfensterAnzeigen, token } = this.props
         const entschluesseltesToken = tokenEntschluesseln(token)
 
+        // Im Modalfenster wird die 'EmailAktualisieren'-Komponente gerendert, wobei die bisherige Emailadresse als Platzhalter im Eingabefeld dient.
         setzeInhaltFuerZentriertesModalfenster(
             "Email aktualisieren",
             <EmailAktualisieren bisherigeEmail={entschluesseltesToken.email || ""} />,
@@ -29,11 +32,12 @@ class AccountEinstellungen extends Component {
         zentriertesModalfensterAnzeigen()
     }
 
-    // Das modale Fenster zur Änderung des Passwortes wird geöffnet,
+    // Das Modalfenster zur Änderung des Passwortes wird geöffnet,
     // wenn der Nutzer auf den entsprechenden Button drückt.
     onPasswortAendern = () => {
         const { setzeInhaltFuerZentriertesModalfenster, zentriertesModalfensterAnzeigen } = this.props
 
+        // Im Modalfenster wird die `PasswortAendern`-Komponente gerendert.
         setzeInhaltFuerZentriertesModalfenster(
             "Passwort ändern",
             <PasswortAendern />,
@@ -42,11 +46,12 @@ class AccountEinstellungen extends Component {
         zentriertesModalfensterAnzeigen()
     }
 
-    // Das modale Fenster zum Löschen des Accounts wird geöffnet,
+    // Das Modalfenster zum Löschen des Accounts wird geöffnet,
     // wenn der Nutzer auf den entsprechenden Button drückt.
     onAccountloeschen = () => {
         const { setzeInhaltFuerOberesModalfenster, oberesModalfensterAnzeigen } = this.props
 
+        // Im Modalfenster wird der Nutzer gewarnt, dass er hiermit seinen Account löscht und muss bestätigen, dass er fortfahren möchte.
         setzeInhaltFuerOberesModalfenster("Achtung", "Alle Daten werden gelöscht! Möchtest du wirklich fortfahren?",
             // Durch die rote Farbe (variant: "danger") wird der Nutzer nochmal auf die Ernsthaftigkeit der Lage hingewiesen.
             [{ name: "Ja", variant: "danger", onClick: this.onDeleteAccept  }, { name: "Schließen", variant: "secondary"} ])
@@ -55,12 +60,12 @@ class AccountEinstellungen extends Component {
 
     // Wenn der Nutzer sich nun dazu entscheidet, seinen Account zu löschen,
     onDeleteAccept = async () => {
-        // Die Eigenschaften von props werden destrukturiert und sind somit separate Variablen.
+        // werden die Eigenschaften von props destrukturiert und sind somit zu separaten Variablen.
         // Somit wird der Präfix "this.props" nicht benötigt.
         const { token, history, setzeInhaltFuerOberesModalfenster, oberesModalfensterAnzeigen, authentifizierungsTokenFestlegen, oberesModalfensterAusblenden } = this.props
         // Diese Funktion löscht den Account des Nutzers auf dem Server und gibt ein Objekt mit einem Fehler oder dem Status zurück.
         const accountLoeschung = await accountVomServerLoeschen(token)
-        // Wenn es einen Fehler beim Löschvorgan des Acconts gibt, wird dieser in einem modalen Fenster angezeigt.
+        // Wenn es einen Fehler beim Löschvorgan des Accounts gibt, wird dieser in einem Modalfenster angezeigt.
         if (accountLoeschung.error) {
             setzeInhaltFuerOberesModalfenster("Fehler", accountLoeschung.error, [{ name: "Schließen", variant: "primary" }])
             oberesModalfensterAnzeigen()
@@ -75,7 +80,7 @@ class AccountEinstellungen extends Component {
     // Definition der "render"-Funktion, die das Aussehen der Komponente definiert.
     // Die Accounteinstellungen werden gerendert.
     render() {
-        // Die Variable "token" wird aus den Props des Komponenten gelesen.
+        // Die Variable "token" wird aus den Props der Komponente gelesen.
         const { token } = this.props
         // Das entschlüsselte Token wird aus dem "token" Wert erstellt.
         const entschluesseltesToken = tokenEntschluesseln(token)
@@ -84,6 +89,7 @@ class AccountEinstellungen extends Component {
             <Container style={{ maxWidth: "100%" }}>
                 <Row>
                     {/* Die Überschrift der Accounteinstellungen wird angezeigt. */}
+                    {/* Um die 'classNames' zu verstehen, empfiehlt sich die Bootstrap-Dokumentation. mt ist eine Abkürzung für marginTop */}
                     <h1 className="mt-4">Accounteinstellungen</h1>
                 </Row>
                 <Row>
@@ -112,16 +118,16 @@ class AccountEinstellungen extends Component {
     }
 }
 
-// mapStateToProps ist eine Funktion, die den Redux Store auf den aktuellen Zustand des Komponenten abbildet.
-// In diesem Fall wird nur das "token" aus dem "authentifizierung"-Slice des Stores extrahiert und als Prop an den Komponenten weitergegeben.
+// mapStateToProps ist eine Funktion, die den aktuellen Zustand des Redux-Stores auf die Eigenschaften der Komponente mapped (abbildet).
+// In diesem Fall wird nur das "token" aus dem Store extrahiert und als Prop an den Komponenten weitergegeben.
 const mapStateToProps = state => {
     return {
         token: state.authentifizierung.token
     }
 }
 
-// mapDispatchToProps ist eine Funktion, die das Dispatch-Objekt aus dem Store erhält und eine Funktionen-Mapping auf den Component-Props definiert.
-// In diesem Fall werden verschiedene Aktionen-Dispatcher definiert, die als Props an den Component weitergegeben werden.
+// mapDispatchToProps ist eine Funktion, die das Dispatch-Objekt aus dem Store erhält und eine Funktionen-Mapping auf den Komponenten-Props definiert.
+// In diesem Fall werden verschiedene Aktionserzeuger definiert, die als Props an die Komponente weitergegeben werden.
 const mapDispatchToProps = dispatch => {
     return {
         setzeInhaltFuerZentriertesModalfenster: (titel, inhalt, buttons) => dispatch(setzeInhaltFuerZentriertesModalfenster(titel, inhalt, buttons)),
@@ -134,5 +140,5 @@ const mapDispatchToProps = dispatch => {
 }
 
 // Der "Accounteinstellungen"-Komponent wird mit dem Redux Store verbunden und mit den oben definierten mapStateToProps und mapDispatchToProps Funktionen verknüpft.
-// Mit withRouter wird der Komponent auch mit dem React Router verbunden, um Zugriff auf den Router-Props (z.B. "history") zu erhalten.
+// Mit withRouter wird der Komponent auch mit dem React Router verbunden, um Zugriff auf die Router-Props (z.B. "history") zu erhalten.
 export default connect(mapStateToProps, mapDispatchToProps)(withRouter(AccountEinstellungen))
