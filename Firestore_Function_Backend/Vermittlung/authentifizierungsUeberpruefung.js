caa942091cc58de2cf5d6aa92092230d164e97ff
algerr <65679099+algerr@users.noreply.github.com>
2023-04-15 22:44:09 +0200

    Update authentifizierungsUeberpruefung.js

--- Firestore_Function_Backend/Vermittlung/authentifizierungsUeberpruefung.js
+++ Firestore_Function_Backend/Vermittlung/authentifizierungsUeberpruefung.js
@@ -1,13 +1,13 @@
-// Importieren des 'jsonwebtoken'-Moduls für die Verarbeitung von JSON-Web-Token
+// Importieren des 'jsonwebtoken'-Moduls für die Verarbeitung von JSON-Web-Tokens.
 const jwt = require('jsonwebtoken')
 
-// Middleware für die Überprüfung der Autorisierung des Benutzers mit JWT
+// Definition der Vermittlungsfunktion zum Authentifizieren des Benutzers.
 module.exports = (req, res, next) => {
 
     // Der Authorization-Headers wird aus der HTTP-Anfrage extrahiert.
     const Authorization_Header = req.get('Authorization')
 
-    // Wenn kein Authorization-Header vorhanden ist, ist der Benutzer nicht authentifiziert
+    // Wenn kein Authorization-Header vorhanden ist, ist der Benutzer nicht authentifiziert.
     if (!Authorization_Header) {
         req.authentifizierungsUeberpruefung = false
         return next()
@@ -16,14 +16,14 @@ module.exports = (req, res, next) => {
     // Das Tokens wird aus dem Authorization-Header extrahiert.
     const token = Authorization_Header.split(' ')[1]
     if (!token || token === '') {
-        // Wenn kein Token vorhanden ist, ist der Benutzer nicht authentifiziert
+        // Wenn kein Token vorhanden ist, ist der Benutzer nicht authentifiziert.
         req.authentifizierungsUeberpruefung = false
         return next()
     }
 
     let entschluesseltesToken
     try {
-        // Verifizieren des Tokens und entschluesseln der Daten (z.B. Benutzername)
+        // Verifizieren des Tokens und Entschlüsselung der Daten (z.B. Benutzername).
         entschluesseltesToken = jwt.verify(token, process.env.TOKEN_SECRET)
     } catch (error) {
         // Wenn das Verifizieren fehlschlägt, ist der Benutzer nicht authentifiziert.
@@ -32,7 +32,7 @@ module.exports = (req, res, next) => {
     }
 
     if(!entschluesseltesToken) {
-        // Wenn kein dekodiertes Token vorhanden ist, ist der Benutzer nicht authentifiziert.
+        // Wenn kein entschlüsseltes Token vorhanden ist, ist der Benutzer nicht authentifiziert.
         req.authentifizierungsUeberpruefung = false
         return next()
     }
