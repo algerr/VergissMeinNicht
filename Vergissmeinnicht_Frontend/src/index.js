edd3fc4f6277182e3ec6c8b72876f595e210634d
algerr <65679099+algerr@users.noreply.github.com>
2023-04-15 22:31:10 +0200

    Update index.js

--- Vergissmeinnicht_Frontend/src/index.js
+++ Vergissmeinnicht_Frontend/src/index.js
@@ -1,9 +1,10 @@
+// Die benötigten Module und Bibliotheken sowie das Styling (index.css) werden importiert.
 import React from 'react'
 import ReactDOM from 'react-dom'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/css/bootstrap-reboot.min.css'
 import 'jquery/dist/jquery.min.js'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './index.css'
 import App from './App'
 
